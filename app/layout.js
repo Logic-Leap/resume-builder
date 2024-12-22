093de43978b677aee5b11a7b2d8643af1d320165
Logic-leap <logicleap00@gmail.com>
2024-12-23 00:19:46 +0530

    navbar added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
 import SessionProviderWrapper from "@/lib/SessionProviderWrapper";
 import "./globals.css";
 
+export const metadata = {
+  title: "Resume Builder",
+  discreption : "start building your resume"
+}
+
 
 export default function RootLayout({ children }) {
   return (
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Navbar from "@/components/Navbar/Navbar";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
@@ -11,13 +12,16 @@ export default function Home() {
   // }
 
   return (
+    <>
+    <Navbar />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello world this is new project by zedro/razal
+
       {session ? (
         <button onClick={() => signOut()}>Sign Out in {session.user.email} </button>
       ) : (
         <button onClick={() => signIn("github")}>Sign in with Github</button>
       )}
     </div>
+    </>
   );
 }
